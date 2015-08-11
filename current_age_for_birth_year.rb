364605e1fee29c47395db45e8a456d51eedb1a30
George Toothman <george.toothman@gmail.com>
2015-08-11 10:53:40 -0400

    refactor tests for time class

--- current_age_for_birth_year.rb
+++ current_age_for_birth_year.rb
@@ -3,6 +3,10 @@ def current_age_for_birth_year(x)
   2015 - x
 end
 
+# def current_age_for_birth_year(birth_year)
+#   2015 - birth_year
+# end
+
 def current_age_for_birth_year(birth_year)
-  2015 - birth_year
+  Time.now.year - birth_year
 end
